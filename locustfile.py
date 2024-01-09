f30221c239bebe7c826492255d760e805374a0a2
Onkar JINDE <71400604+onkarjinde1@users.noreply.github.com>
2024-01-09 15:27:38 +0100

    locustfile.py

--- locustfile.py
+++ locustfile.py
@@ -2,7 +2,7 @@ from locust import HttpUser, task, between
 
 class MyLocust(HttpUser):
     wait_time = between(1, 3)
-    host = "http://127.0.0.1:5000/numericalintegralservice/0/3.14"  # Replace with the actual URL of your application
+    host = "http://4.227.214.13:5000/numericalintegralservice/0/3.14"  # Replace with the actual URL of your application
 
     @task(1)  # Adjust the weights as needed
     def numerical_integration_task(self):
